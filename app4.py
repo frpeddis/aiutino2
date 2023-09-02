f6b7f51b659771619fec6c4e356b73adec11cc19
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-09-02 17:09:28 +0200

    Update app4.py

--- app4.py
+++ app4.py
@@ -9,39 +9,41 @@ import openai
 import pytesseract
 
 # Initialize GPT API (Replace with your actual API key)
-openai.api_key = st.secrets["API_KEY"]
+#openai.api_key = st.secrets["API_KEY"]
 
 # Set tesseract cmd path
 pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
+VERSION = "0.7.0"
 
+st.set_page_config(
+    page_title="Aiutino",
+    page_icon="🖼️",
+    layout="wide",
+)
+
+# ---------- HEADER ----------
 st.title("🖼️ Welcome to Aiutino!")
-
-def analyze_text_with_gpt3(text):
-    prompt = f"This is a text to analyze: {text}. First think step by step, then understand what is the topic of the text. You are an expert on that topic. Now give me your opinion about that topic."
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=prompt,
-        max_tokens=200,
-        temperature=0.2
-    )
-    return response.choices[0].text.strip()
 
 # Image loading options
 option = st.radio(
-    "Upload an image, take one with your camera, or load image from a URL",
-    ("Upload an image ⬆️", "Take a photo with my camera 📷", "Load image from a URL 🌐"),
+    label="Upload an image, take one with your camera, or load image from a URL",
+    options=("Upload an image ⬆️", "Take a photo with my camera 📷", "Load image from a URL 🌐"),
 )
 
-upload_img = None
 if option == "Take a photo with my camera 📷":
-    upload_img = st.camera_input("Take a picture")
+    upload_img = st.camera_input(label="Take a picture")
     mode = "camera"
+
 elif option == "Upload an image ⬆️":
-    upload_img = st.file_uploader("Upload an image", type=["bmp", "jpg", "jpeg", "png", "svg"])
+    upload_img = st.file_uploader(
+        label="Upload an image", type=["bmp", "jpg", "jpeg", "png", "svg"]
+    )
     mode = "upload"
+
 elif option == "Load image from a URL 🌐":
     url = st.text_input("Image URL", key="url")
     mode = "url"
+
     if url != "":
         try:
             response = requests.get(url)
@@ -49,24 +51,64 @@ elif option == "Load image from a URL 🌐":
         except:
             st.error("The URL does not seem to be valid.")
 
+# Process image and apply operations
 with contextlib.suppress(NameError):
     if upload_img is not None:
-        pil_img = upload_img.convert("RGB") if mode == "url" else Image.open(upload_img).convert("RGB")
-        img_arr = np.array(pil_img)
-        st.image(img_arr, use_column_width="auto", caption="Uploaded Image")
+        pil_img = (
+            upload_img.convert("RGB")
+            if mode == "url"
+            else Image.open(upload_img).convert("RGB")
+        )
+        img_arr = np.asarray(pil_img)
 
-        degrees = st.slider("Drag slider to rotate image clockwise 🔁", min_value=0, max_value=360, value=0)
+        # Display original image
+        #st.image(img_arr, use_column_width="auto", caption="Uploaded Image")
+
+        # ---------- ROTATE ----------
+        degrees = st.slider(
+            "Drag slider to rotate image clockwise 🔁",
+            min_value=0,
+            max_value=360,
+            value=0,
+            key="rotate_slider",
+        )
         rotated_img = pil_img.rotate(360 - degrees)
+        st.image(
+            rotated_img,
+            use_column_width="auto",
+            caption=f"Rotated by {degrees} degrees clockwise",
+        )
+        #if st.button("↩️ Reset Rotation", use_container_width=True):
+        #    st.success("Rotation reset to original!")
+        
+        # ---------- CROP ----------
+        st.text("Crop image ✂️")
         cropped_img = st_cropper(rotated_img, should_resize_image=True)
-
-        use_cropped = st.checkbox("Use cropped Image?")
-        final_image = cropped_img if use_cropped else rotated_img
-        text = pytesseract.image_to_string(final_image)
+        st.text(
+            f"Cropped width = {cropped_img.size[0]}px and height = {cropped_img.size[1]}px"
+        )
         
+        if st.checkbox(
+            label="Use cropped Image?",
+            help="Select to use the cropped image in further operations",
+            key="crop",
+        ):
+            final_image = cropped_img
+        else:
+            final_image = rotated_img
+        # Perform OCR
         st.write("Recognized Text")
+        text = pytesseract.image_to_string(final_image)
         st.write(text)
+        # ---------- FINAL OPERATIONS ----------
         
-        if st.button('Run GPT-3 Analysis'):
-            gpt3_response = analyze_text_with_gpt3(text)
-            st.write("GPT-3 Analysis")
-            st.write(gpt3_response)
+        
+        #final_image.save("final_image.png")
+
+        #with open("final_image.png", "rb") as file:
+        #    st.download_button(
+        #        "💾 Download final image",
+        #        data=file,
+        #        mime="image/png",
+        #        use_container_width=True,
+        #    )

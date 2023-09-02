695eeff4cfc4a91b8907d1c605ea38dd6590e100
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-09-02 16:21:12 +0200

    Update app4.py

--- app4.py
+++ app4.py
@@ -63,23 +63,21 @@ with contextlib.suppress(NameError):
 
         st.image(final_image, use_column_width="auto", caption="Final Image")
 
-        if st.button("Analyze with ChatGPT - Method 1", key='button1'):
-            prompt = f"This is a text to analyze: {text}. Look for any questions contained in the text..."
-            # rest of the GPT-3 logic for the first button
-            response = openai.Completion.create(
-                engine="text-davinci-002",
-                prompt=prompt,
-                max_tokens=200
-            )
-            
-        if st.button("Analyze with ChatGPT - Method 2", key='button2'):
-            prompt = f"This is a text to analyze: {text}. First think step by step, then understand what is the topic of the text..."
-            # rest of the GPT-3 logic for the second button
+        if st.button('Analyze with ChatGPT'):
+            prompt = f"This is a text to analyze: {text}. First think step by step, then understand what is the topic of the text. You are an expert on that topic. Now give me your opinion about that topic."
             response = openai.Completion.create(
                 engine="text-davinci-002",
                 prompt=prompt,
                 max_tokens=200,
                 temperature=0.2
             )
-            st.write("GPT-3 Analysis - overview")
+            prompt = f"Now look for any questions contained in the text {text}. If you find a question, a quiz, a multiple-choice question, etc., give me the answer you consider correct to that question, quiz, or multiple-choice question. Do not end your output without giving an answer to questions contained in the text."
+            response = openai.Completion.create(
+                engine="text-davinci-002",
+                prompt=prompt,
+                max_tokens=300,
+                temperature=0.2
+            )
+    
+            st.write("GPT-3 Analysis")
             st.write(response.choices[0].text.strip())

6f39d1f7d0a8e48b936e7177920b273faf8365da
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-09-02 12:42:31 +0200

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,3 +1,4 @@
+import contextlib
 from io import BytesIO
 import numpy as np
 import requests
@@ -5,39 +6,35 @@ import streamlit as st
 from PIL import Image
 from streamlit_cropper import st_cropper
 
+VERSION = "0.7.0"
+
 st.set_page_config(
     page_title="Aiutino",
     page_icon="🖼️",
     layout="wide",
 )
 
+# ---------- HEADER ----------
 st.title("🖼️ Welcome to Aiutino!")
 
-# Upload options
+# Image loading options
 option = st.radio(
     label="Upload an image, take one with your camera, or load image from a URL",
-    options=(
-        "Upload an image ⬆️",
-        "Take a photo with my camera 📷",
-        "Load image from a URL 🌐",
-    )
+    options=("Upload an image ⬆️", "Take a photo with my camera 📷", "Load image from a URL 🌐"),
 )
 
 if option == "Take a photo with my camera 📷":
-    upload_img = st.camera_input(
-        label="Take a picture",
-    )
+    upload_img = st.camera_input(label="Take a picture")
     mode = "camera"
 
 elif option == "Upload an image ⬆️":
     upload_img = st.file_uploader(
-        label="Upload an image",
-        type=["bmp", "jpg", "jpeg", "png", "svg"],
+        label="Upload an image", type=["bmp", "jpg", "jpeg", "png", "svg"]
     )
     mode = "upload"
 
 elif option == "Load image from a URL 🌐":
-    url = st.text_input("Image URL")
+    url = st.text_input("Image URL", key="url")
     mode = "url"
 
     if url != "":
@@ -47,7 +44,8 @@ elif option == "Load image from a URL 🌐":
         except:
             st.error("The URL does not seem to be valid.")
 
-if 'upload_img' in locals():
+# Process image and apply operations
+with contextlib.suppress(NameError):
     if upload_img is not None:
         pil_img = (
             upload_img.convert("RGB")
@@ -56,29 +54,10 @@ if 'upload_img' in locals():
         )
         img_arr = np.asarray(pil_img)
 
+        # Display original image
         st.image(img_arr, use_column_width="auto", caption="Uploaded Image")
-        st.text(
-            f"Original width = {pil_img.size[0]}px and height = {pil_img.size[1]}px"
-        )
 
-        # Crop Feature
-        st.text("Crop image ✂️")
-        cropped_img = st_cropper(Image.fromarray(img_arr), should_resize_image=True)
-        st.text(
-            f"Cropped width = {cropped_img.size[0]}px and height = {cropped_img.size[1]}px"
-        )
-
-        # Use Cropped Image or Original
-        if st.checkbox(
-            label="Use cropped Image?",
-            help="Select to use the cropped image in further operations",
-            key="crop",
-        ):
-            image = cropped_img
-        else:
-            image = Image.fromarray(img_arr)
-
-        # Rotate Feature
+        # ---------- ROTATE ----------
         degrees = st.slider(
             "Drag slider to rotate image clockwise 🔁",
             min_value=0,
@@ -86,12 +65,50 @@ if 'upload_img' in locals():
             value=0,
             key="rotate_slider",
         )
-        rotated_img = image.rotate(360 - degrees)
+        rotated_img = pil_img.rotate(360 - degrees)
         st.image(
             rotated_img,
             use_column_width="auto",
             caption=f"Rotated by {degrees} degrees clockwise",
         )
-
-        if st.button("↩️ Reset Rotation"):
+        if st.button("↩️ Reset Rotation", use_container_width=True):
             st.success("Rotation reset to original!")
+        
+        # ---------- CROP ----------
+        st.text("Crop image ✂️")
+        cropped_img = st_cropper(rotated_img, should_resize_image=True)
+        st.text(
+            f"Cropped width = {cropped_img.size[0]}px and height = {cropped_img.size[1]}px"
+        )
+        
+        if st.checkbox(
+            label="Use cropped Image?",
+            help="Select to use the cropped image in further operations",
+            key="crop",
+        ):
+            final_image = cropped_img
+        else:
+            final_image = rotated_img
+
+        # ---------- FINAL OPERATIONS ----------
+        lcol, rcol = st.columns(2)
+        lcol.image(
+            img_arr,
+            use_column_width="auto",
+            caption=f"Original Image ({pil_img.size[0]} x {pil_img.size[1]})",
+        )
+        rcol.image(
+            final_image,
+            use_column_width="auto",
+            caption=f"Final Image ({final_image.size[0]} x {final_image.size[1]})",
+        )
+        
+        final_image.save("final_image.png")
+
+        with open("final_image.png", "rb") as file:
+            st.download_button(
+                "💾 Download final image",
+                data=file,
+                mime="image/png",
+                use_container_width=True,
+            )

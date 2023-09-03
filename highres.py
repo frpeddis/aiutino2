b6d3f95fee087ecb312b34b7efb8dd85ed2c7c7c
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-09-03 21:32:03 +0200

    Update highres.py

--- highres.py
+++ highres.py
@@ -12,41 +12,11 @@ st.set_page_config(
 
 st.title("High-Resolution Image Capture Example")
 
+# Embed the HTML page
 st.markdown(
-    """
-    <script>
-    async function captureHighQualityImage() {
-        const video = document.createElement('video');
-        video.style.display = 'none';
-        const stream = await navigator.mediaDevices.getUserMedia({video: {facingMode: 'user', width: {ideal: 4096}, height: {ideal: 2160}}});
-        document.body.appendChild(video);
-        video.srcObject = stream;
-        await video.play();
-        const canvas = document.createElement('canvas');
-        canvas.width = video.videoWidth;
-        canvas.height = video.videoHeight;
-        canvas.getContext('2d').drawImage(video, 0, 0);
-        stream.getTracks().forEach(track => track.stop());
-        const imgDataUrl = canvas.toDataURL('image/jpeg', 1.0);
-        document.body.removeChild(video);
-        const inputElement = document.createElement('textarea');
-        inputElement.value = imgDataUrl;
-        inputElement.setAttribute('id', 'base64input');
-        document.body.appendChild(inputElement);
-    }
-    </script>
-    """,
+    f'<iframe src="camera.html" width="700" height="600"></iframe>',
     unsafe_allow_html=True,
 )
-
-# Capture Button
-capture_button = st.button("Capture High-Res Image")
-
-if capture_button:
-    st.write(
-        "<script type='text/javascript'>captureHighQualityImage()</script>",
-        unsafe_allow_html=True,
-    )
 
 # Hidden input to store base64 string
 img_data_url = st.text_area("Image Data URL", "", key="base64input", max_chars=None, height=0)

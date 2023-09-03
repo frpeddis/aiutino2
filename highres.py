39f3d7d750e1c296759e99998be2fe7aa11fa1f1
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-09-03 21:41:19 +0200

    Update highres.py

--- highres.py
+++ highres.py
@@ -1,47 +1,18 @@
 import streamlit as st
-from PIL import Image
-import base64
-from io import BytesIO
+import cv2
+import numpy as np
 
-st.title("High-Resolution Image Capture Example")
+img_file_buffer = st.camera_input("Take a picture")
 
-# JavaScript to handle high-res image capture
-js_script = """
-    <script>
-        async function captureImage() {
-            const video = document.createElement('video');
-            video.style.display = 'none';
-            const stream = await navigator.mediaDevices.getUserMedia({video: {facingMode: 'user', width: {ideal: 4096}, height: {ideal: 2160}}});
-            video.srcObject = stream;
-            await video.play();
-            
-            // Resize the output to fit the video element.
-            google.colab.output.setIframeHeight(document.documentElement.scrollHeight, true);
-            
-            const canvas = document.createElement('canvas');
-            canvas.width = video.videoWidth;
-            canvas.height = video.videoHeight;
-            canvas.getContext('2d').drawImage(video, 0, 0);
-            stream.getVideoTracks()[0].stop();
-            return canvas.toDataURL('image/jpeg', quality=1.0);
-        }
-    </script>
-"""
+if img_file_buffer is not None:
+    # To read image file buffer with OpenCV:
+    bytes_data = img_file_buffer.getvalue()
+    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
 
-# Embed the JavaScript code
-st.markdown(js_script, unsafe_allow_html=True)
+    # Check the type of cv2_img:
+    # Should output: <class 'numpy.ndarray'>
+    st.write(type(cv2_img))
 
-# Button to capture image
-if st.button("Capture from Camera"):
-    js = "captureImage().then(dataUrl => document.getElementById('img_data').value = dataUrl);"
-    st.markdown('<input type="hidden" id="img_data">', unsafe_allow_html=True)
-    st.markdown(f"<button onclick='{js}'>Capture Image</button>", unsafe_allow_html=True)
-
-# Hidden input to store base64 string
-img_data_url = st.text_area("Image Data URL", "", key="base64input", max_chars=None, height=0)
-
-if img_data_url:
-    # Remove data URL prefix and convert base64 data to PIL Image
-    img_data = base64.b64decode(img_data_url.split(",")[1])
-    img = Image.open(BytesIO(img_data))
-    st.image(img, caption="Captured High-Resolution Image", use_column_width=True)
+    # Check the shape of cv2_img:
+    # Should output shape: (height, width, channels)
+    st.write(cv2_img.shape)
